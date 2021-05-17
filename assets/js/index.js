e4aab79ce02cb2570f02f1aaa767e39960974d22
cristian <cristiannicolas.m1@gmail.com>
2021-05-17 00:01:20 -0400

    Se le agregan elementos como tooltip, modales en las cards

---
+++ assets/js/index.js
@@ -0,0 +1,24 @@
+$(function(){
+    $("a").click(function(event){
+        event.preventDefault();
+
+        var gato = this.hash;
+
+        $("html").animate(
+            {
+                scrollTop: $(gato).offset().top - 86,
+            },
+            800
+        );
+    });
+
+
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+      })
+
+      $(function () {
+        $('[data-toggle="popover"]').popover()
+      })
+      
+});
